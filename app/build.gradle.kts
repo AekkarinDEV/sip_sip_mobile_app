82d5c340c6962c479647e0b089e54d495f87b16b
kixmbap <pwathkim@gmail.com>
2026-02-05 19:36:19 +0700

    Page UserSetup
    หน้ากรอกข้อมูลที่จำเป้นเมื่อเข้าสู่ระบบครั้งแรก

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,5 +47,8 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
 }
