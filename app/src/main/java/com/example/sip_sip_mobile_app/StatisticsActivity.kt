0e7ca3d2583ca472a4cc9a9881dea296a8db9dde
AekkarinDEV <ezio4227@gmail.com>
2026-02-17 23:20:08 +0700

    fix: main and static page error after nav

--- app/src/main/java/com/example/sip_sip_mobile_app/StatisticsActivity.kt
+++ app/src/main/java/com/example/sip_sip_mobile_app/StatisticsActivity.kt
@@ -3,12 +3,19 @@ package com.example.sip_sip_mobile_app
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class StatisticsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         // Setup bottom navigation
         val bottomNavView = findViewById<View>(R.id.layout_bottom_nav)
