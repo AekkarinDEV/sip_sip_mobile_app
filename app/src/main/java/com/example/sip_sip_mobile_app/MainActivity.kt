e6c8cfff940946939e80cd8e4961e66b5a7665b7
AekkarinDEV <ezio4227@gmail.com>
2026-02-17 23:32:17 +0700

    feat: fecth user name on main page

--- app/src/main/java/com/example/sip_sip_mobile_app/MainActivity.kt
+++ app/src/main/java/com/example/sip_sip_mobile_app/MainActivity.kt
@@ -2,12 +2,21 @@ package com.example.sip_sip_mobile_app
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,9 +27,37 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+
         // Setup bottom navigation
         val bottomNavView = findViewById<View>(R.id.layout_bottom_nav)
         val bottomNavManager = BottomNavManager(this, bottomNavView)
         bottomNavManager.setupBottomNavigation()
+
+        loadUserProfile()
+    }
+
+    private fun loadUserProfile() {
+        val user = auth.currentUser ?: return
+
+        val tvUsername = findViewById<TextView>(R.id.tvUsername)
+        val imgAvatar = findViewById<ImageView>(R.id.imgAvatar)
+
+        db.collection("users").document(user.uid).get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    val name = document.getString("name") ?: ""
+                    val avatarUrl = document.getString("avatarUrl")
+
+                    tvUsername.text = name
+
+                    if (!avatarUrl.isNullOrEmpty()) {
+                        Glide.with(this)
+                            .load(avatarUrl)
+                            .into(imgAvatar)
+                    }
+                }
+            }
     }
 }
